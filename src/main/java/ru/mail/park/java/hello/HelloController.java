734ae1042e503a79061cadba9398da4269e15535
Ivan Sopov <sopov.ivan@gmail.com>
2016-08-17 14:52:59 +0300

    allow CORS for single resource only

--- src/main/java/ru/mail/park/java/hello/HelloController.java
+++ src/main/java/ru/mail/park/java/hello/HelloController.java
@@ -1,6 +1,7 @@
 package ru.mail.park.java.hello;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -9,6 +10,7 @@ public class HelloController {
 
 	@ResponseBody
 	@RequestMapping("/")
+	@CrossOrigin(origins = "https://isopov.github.io")
 	public String hello(){
 		return "Hello Park!";
 	}
